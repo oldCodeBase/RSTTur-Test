7fae28460e8e3564d4723d8625a684ad7a7ef001
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-08-04 11:00:12 +0300

    Initial Commit

---
+++ RSTTur-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RSTTur-Test
+//
+//  Created by Ibragim Akaev on 04/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RSTTur-Test/RSTTur_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSTTur_TestApp.swift
+//  RSTTur-Test
+//
+//  Created by Ibragim Akaev on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct RSTTur_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
